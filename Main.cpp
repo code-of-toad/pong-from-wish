38b70154654a94bde49aa2fc667a2c651518ad5c
Danny Han <codeoftoad@gmail.com>
2024-05-28 13:38:46 -0400

    Initial project setup

---
+++ Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using std::cout, std::endl, std::string;
+
+int main() {
+    cout << "hello world\n";
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sfml-project-whatever
+
+Project made to facilitate free experimentation with SFML.
+
+May change into something else later.
